d056c60100db308c46f3cb790a1ecd9ba537966b
AndreyXrey <77732453+AndreyXrey@users.noreply.github.com>
2022-10-11 04:50:42 +0300

    Второе видео

---
+++ README.md
@@ -0,0 +1,3 @@
+Вот этот курс
+
+https://youtube.com/playlist?list=PLg0FCxkXe7zPDi2DHx4B5MUAv4u18EZMu
--- fuck_function.py
+++
@@ -1,9 +0,0 @@
-def fuck(you):
-    print(you + ' will manage to become programmer')
-
-
-print('Enter you name below:')
-YOU = input()
-
-if __name__ == "__main__":
-    fuck(YOU)
